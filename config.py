a3b320a7d68cf7e7112020e86f3912e0de598f50
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-14 16:20:05 -0300

    Procfile e gunicorn

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import pymysql
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dbjustcook'
+SQLALCHEMY_DATABASE_URI = 'mysql://b3181b88ca232f:9d9b220c@us-cdbr-east-05.cleardb.net/heroku_1047d87c3352696?reconnect=true'
 # SQLAlchemy monitorará modificações de objetos
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
