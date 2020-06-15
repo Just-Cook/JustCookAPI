b44764cbe8f9df3a9f1dd3177edafde1b44191a0
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-15 16:12:31 -0300

    uninstall pymysql

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 #CLEARDB_DATABASE_URL = 'mysql://b640e097c489c1:cf988b90@us-cdbr-east-05.cleardb.net/heroku_9332df0a49f4d18?reconnect=true'
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dbjustcook'
-SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://b640e097c489c1:cf988b90@us-cdbr-east-05.cleardb.net/heroku_9332df0a49f4d18?reconnect=true'
+SQLALCHEMY_DATABASE_URI =  'mysql://b640e097c489c1:cf988b90@us-cdbr-east-05.cleardb.net/heroku_9332df0a49f4d18?reconnect=true'
 # SQLAlchemy monitorará modificações de objetos
 SQLALCHEMY_TRACK_MODIFICATIONS = True
