7a8b7a644430e78fb0085f243c4f7643bc90d1dc
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-15 17:01:18 -0300

    reconnect

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dbjustcook'
-SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://bebe8d06f7da0b:40bca22a@us-cdbr-east-05.cleardb.net/heroku_baa6c8c336f761a?reconnect=true'
+SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://bebe8d06f7da0b:40bca22a@us-cdbr-east-05.cleardb.net/heroku_baa6c8c336f761a'
 # SQLAlchemy monitorará modificações de objetos
 SQLALCHEMY_TRACK_MODIFICATIONS = True
