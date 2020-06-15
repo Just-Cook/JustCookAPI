cf226419dcea17b0a1de102ef8b94fe59f545307
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-15 11:41:15 -0300

    upgrade sqlAchemy-database-uri

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import pymysql
 
 
-#CLEARDB_DATABASE_URL = 'mysql://b3181b88ca232f:9d9b220c@us-cdbr-east-05.cleardb.net/heroku_1047d87c3352696?reconnect=true'
-DATABASE_URL = 'mysql+pymysql://root:@localhost/dbjustcook'
+#CLEARDB_DATABASE_URL = 'mysql://b640e097c489c1:cf988b90@us-cdbr-east-05.cleardb.net/heroku_9332df0a49f4d18?reconnect=true'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dbjustcook'
+SQLALCHEMY_DATABASE_URI =  'mysql://b640e097c489c1:cf988b90@us-cdbr-east-05.cleardb.net/heroku_9332df0a49f4d18?reconnect=true'
 # SQLAlchemy monitorará modificações de objetos
 SQLALCHEMY_TRACK_MODIFICATIONS = True
