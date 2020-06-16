918c835370f7fc278f65ef20dc7a5109f8c062fc
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-16 09:49:24 -0300

    config SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dbjustcook'
-SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://b5d50d51499f64:5dd68b20@us-cdbr-east-05.cleardb.net/heroku_4fb1a6fda367bda?reconnect=true'
+SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://b5d50d51499f64:5dd68b20@us-cdbr-east-05.cleardb.net/heroku_4fb1a6fda367bda'
 # SQLAlchemy monitorará modificações de objetos
 SQLALCHEMY_TRACK_MODIFICATIONS = True
