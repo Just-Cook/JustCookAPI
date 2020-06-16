fdf0470d8609462b43ad01560b698aa91738d8c4
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-16 11:01:14 -0300

    created ingrediente model

---
+++ app_justcook/models/ingrediente.py
@@ -0,0 +1,41 @@
+from app_justcook import db
+
+
+class IngredienteModel(db.Model):
+    __tablename__ = 'ingrediente'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    nome = db.Column(db.String(100))
+    quantidade = db.Column(db.Float(precision=1))
+    unidade = db.Column(db.String(7))
+    receita_id = db.Column(db.Integer, nullable=False)
+
+
+    def __init__(self, nome, quantidade, unidade, receita_id):
+        self.nome = nome
+        self.quantidade = quantidade
+        self.unidade = unidade
+        self.receita_id = receita_id
+
+    def json(self):
+        return {
+            'id':self.id,
+            'nome':self.nome,
+            'quantidade':self.quantidade,
+            'unidade':self.unidade,
+            'receita_id':self.receita_id
+
+        }
+
+    @classmethod
+    def find_by_id(cls, id):
+        ingrediente = cls.query.filter_by(id=id).first()
+
+        if(ingrediente):
+            return ingrediente
+        return None
+
+    @classmethod
+    def find_all(cls, id):
+        ingredientes = cls.query.all()
+        return {'ingredientes':[ingrediente.json() for ingrediente in ingredientes]}
