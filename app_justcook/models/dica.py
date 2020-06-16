10c2287d0e418a3d6c92ce7ef9c982d58bd2bc4b
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-16 11:00:44 -0300

    created dicas model

---
+++ app_justcook/models/dica.py
@@ -0,0 +1,35 @@
+from app_justcook import db
+
+
+class DicaModel(db.Model):
+    __tablename__ = 'dica'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    descricao = db.Column(db.Text)
+    passo_id = db.Column(db.Integer, nullable=False)
+
+
+    def __init__(self, descricao, passo_id):
+        self.descricao = descricao
+        self.passo_id = passo_id
+
+
+    def json(self):
+        return {
+            'id':self.id,
+            'descricao':self.descricao,
+            'passo_id':self.passo_id
+        }
+
+    @classmethod
+    def find_by_id(cls, id):
+        dica = cls.query.filter_by(id=id).first()
+
+        if(dica):
+            return dica
+        return None
+
+    @classmethod
+    def find_all(cls):
+        dicas = cls.query.all()
+        return {'dicas':[dica.json() for dica in dicas]}
