233c6ae7493dbac9836e4b8ce5a5098f9d379882
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-16 11:39:14 -0300

    created passo model

---
+++ app_justcook/models/passo.py
@@ -0,0 +1,41 @@
+from app_justcook import db
+
+class PassoModel(db.Model):
+    __tablename__ = 'passo'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key= True)
+    descricao = db.Column(db.Text)
+    image_name = db.Column(db.String(20))
+    cronometro = db.Column(db.Integer)
+    modulo_id = db.Column(db.Integer, nullable=False)
+
+
+    def __init__(self, descricao, image_name, cronometro, modulo_id):
+        self.descricao = descricao
+        self.image_name = image_name
+        self.cronometro = cronometro
+        self.modulo_id = modulo_id
+
+
+    def json(self):
+        return {
+            'id':self.id,
+            'descricao':self.descricao,
+            'image_name':self.image_name,
+            'cronometro':self.cronometro,
+            'modulo_id':self.modulo_id
+
+        }
+
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_by_id(cls, id):
+        passo = cls.query.filter_by(id=id).first()
+
+        if(passo):
+            return passo
+        return None
