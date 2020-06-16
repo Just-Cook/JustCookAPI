3d33a88cab5ccdde9157ab1c4e0465dc7e98a37b
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-06-16 12:04:57 -0300

    created tecnica model

---
+++ app_justcook/models/tecnica.py
@@ -0,0 +1,35 @@
+from app_justcook import db
+
+
+class TecnicaModel(db.Model):
+    __tablename__='tecnica'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(50))
+    image_name = db.Column(db.String(20))
+
+
+
+    def __init__(self, titulo, image_name):
+        self.titulo = titulo
+        self.image_name = image_name
+
+
+    def json(self):
+        return {
+            'id':self.id,
+            'titulo':self.titulo,
+            'image_name':self.image_name
+        }
+
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_by_id(cls, id):
+        tecnica = cls.query.filter_by(id=id).first()
+        if  tecnica:
+            return tecnica
+        return None
